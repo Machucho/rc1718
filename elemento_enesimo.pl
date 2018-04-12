49a4bc7f728209744ad6490893aa0d039f1c2c6b
Manuel <manuel.limon@alu.uhu.es>
2018-04-12 10:29:00 +0200

    Creado elimina_cada_n.pl

--- elemento_enesimo.pl
+++ elemento_enesimo.pl
@@ -1,7 +1,8 @@
 % elemento_enesimo(+Pos, +Lista, -Elem)
-
 % Es cierto cuando Elem unifica con el
-
-% elemento que ocupa la posición Pos de Lista
-
+% elemento que ocupa la posiciÃ³n Pos de Lista
 % empezando por 0.
+
+elemento_enesimo(0, [E|_], E).
+elemento_enesimo(Pos, [_|Resto], E):- Pos > 0,
+   Pos2 is Pos - 1, elemento_enesimo(Pos2, Resto, E).
