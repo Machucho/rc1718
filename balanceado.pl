1e4dce84bd15e57bb6e02666a62354ad61cd11f6
Manuel <manuel.limon@alu.uhu.es>
2018-04-19 10:30:14 +0200

    Creados balanceado.pl

---
+++ balanceado.pl
@@ -0,0 +1,23 @@
+%balanceado(+A)
+%es cierto si para todo nodo, la diferencia de
+%alturas entr el subarbol izdo y el dcho
+%difiere como maximo una unidad.
+balanceado(nil).
+balanceado(a,(_,HI,HD)):-
+    balanceado(HI),
+    balanceado(HD),
+    altura(HD, RD),
+    R is RI - RD,
+    R2 is abs(R),
+    R2 < 2.
+
+
+%altura(+A, -N)
+%Es cierto cuando N unifica con la altura del 
+%arbol A.
+
+altura(nil,0).
+altura(A(_, HI, HD), R2):- altura(HI, RI),
+                         altura(HD, RD),
+                         R is max(RI, RD),
+                         R2 is R + 1.
